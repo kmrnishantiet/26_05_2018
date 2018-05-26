243b5ab8d7eeb18b9978c9b3b15b4a41ddbbaaeb
Kumar <kmrnishantiet@gmail.com>
2018-05-27 01:02:18 +0530

    Test1 1st commit

--- Test1.java
+++ Test1.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.function.BinaryOperator;
 import java.util.stream.Stream;
 
-interface A{
+interface B{
 	
 	default Object clone() throws CloneNotSupportedException {
 		
